0225372173d091d04fd1af85d93668b8750245f6
HKimAnh <90829752+HKimAnh@users.noreply.github.com>
2021-10-26 10:22:30 +0700

    Update navh.php

--- navh.php
+++ navh.php
@@ -5,10 +5,10 @@
                 <p style="color:#fcc39b; margin-left:20px; margin-bottom:0">FREE SHIPING FOR ORDERS OVER $50</p>
                 <ul class="navbar-nav ms-auto mb-2 mb-1g-0" style="margin-bottom:0 !important">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#"><i class="fas fa-sign-in-alt"></i> Đăng nhập</a>
+                        <a class="nav-link active" href="#"><i class="fas fa-sign-in-alt"></i> Log in</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="#"><i class="fas fa-envelope"></i> Liên hệ</a>
+                        <a class="nav-link active" href="#"><i class="fas fa-envelope"></i> Contact</a>
                 </ul>
             </div>
 
