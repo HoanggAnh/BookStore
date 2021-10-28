153f03e83bec4c3ea4b476a3bbfa257d4fe89bf6
nguyenchamanh1705 <91042731+nguyenchamanh1705@users.noreply.github.com>
2021-10-28 20:42:43 +0700

    Create khongcosanpham.php

---
+++ khongcosanpham.php
@@ -0,0 +1,35 @@
+<?php 
+	include "login.php"
+	?>
+<?php 
+	include "header.php"
+	?>
+<?php 
+	include "navh.php"
+    ?>
+    <?php 
+	include "navbar.php"
+	?>
+	
+
+	<div id="page-content" class="single-page">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<ul class="breadcrumb">
+						<li><a href="index.php">Trang chủ</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading"><h1 style="color:red">There are no books in your cart!</h1></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php 
+	include "footer.php"
+	?>
+</body>
+</html>
